7891fdc0d9fc42a975e4ffc7416b8aa84c283228
Beata Kosmider <kosmibe@gmail.com>
2023-03-18 09:08:41 +0100

    task is done

--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
 
-function Input() {
-  return
+function Input({ onInputChange }) {
+  return (
+    <input
+      type="text"
+      onChange={(e) => onInputChange(e.target.value)}
+      placeholder="Wpisz tu coś ładnego!"
+    />
+  );
 }
-
 export default Input;
+/*Utwórz komponent Input (<input> typu text), który będzie przyjmował prop:
+
+onInputChange - funkcję callbackową, która po każdym wpisanym znaku w pole <input>, przekaże wartość (value) inputu do komponentu rodzica i użyje w console.log().
+
+Komponent Input powinien zwracać tag <input>.
+
+Prop musi być przekazany przez rodzica. (<Input />)*/
